e8105b12b89bfd84933ff03ebc469a6cecacafc9
CockpitConnect <51696821+CockpitConnect@users.noreply.github.com>
2021-07-19 23:00:10 +0200

    Update pull_down.js

--- examples/pull_down.js
+++ examples/pull_down.js
@@ -3,11 +3,15 @@ const MCP23017 = require('node-mcp23017')
 const mcp = new MCP23017({
     address: 0x20, //all address pins pulled low
     device: 1, // Model B
-    debug: false
+    debug: true
 })
 
+mcp.pinMode(5, mcp.INPUT);
 mcp.pinMode(6, mcp.INPUT);
+mcp.pinMode(7, mcp.INPUT);
 
 setInterval(function() {
-    mcp.digitalRead(6, (error, value) => console.log(error, value));
+    mcp.digitalRead(5, (error, value) => console.log("pin 5", error, value));
+    mcp.digitalRead(6, (error, value) => console.log("pin 6", error, value));
+    mcp.digitalRead(7, (error, value) => console.log("pin 7", error, value));
 }, 1000);
