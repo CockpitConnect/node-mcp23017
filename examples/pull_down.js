bf4636f03ce84e72c59b48c346c7c184ac461c95
CockpitConnect <51696821+CockpitConnect@users.noreply.github.com>
2021-07-19 21:19:47 +0200

    Update pull_down.js

--- examples/pull_down.js
+++ examples/pull_down.js
@@ -6,25 +6,8 @@ const mcp = new MCP23017({
     debug: false
 })
 
-mcp.pinMode(0, mcp.INPUT_PULLUP);
-mcp.pinMode(1, mcp.INPUT_PULLUP);
-mcp.pinMode(2, mcp.INPUT_PULLUP);
-mcp.pinMode(3, mcp.INPUT_PULLUP);
-mcp.pinMode(4, mcp.INPUT_PULLUP);
-mcp.pinMode(5, mcp.INPUT_PULLUP);
-mcp.pinMode(6, mcp.INPUT_PULLUP);
-mcp.pinMode(7, mcp.INPUT_PULLUP);
-mcp.pinMode(8, mcp.INPUT_PULLUP);
-mcp.pinMode(9, mcp.INPUT_PULLUP);
-mcp.pinMode(10, mcp.INPUT_PULLUP);
-mcp.pinMode(11, mcp.INPUT_PULLUP);
-mcp.pinMode(12, mcp.INPUT_PULLUP);
-mcp.pinMode(13, mcp.INPUT_PULLUP);
-mcp.pinMode(14, mcp.INPUT_PULLUP);
-mcp.pinMode(15, mcp.INPUT); // this one should float from time to time
+mcp.pinMode(6, mcp.INPUT);
 
 setInterval(function() {1
-    for (let i = 8; i < 16; i++) {
-      mcp.digitalRead(i, (error, value) => console.log(error, value));
-    }
+    mcp.digitalRead(6, (error, value) => console.log(error, value));
 }, 1000);
